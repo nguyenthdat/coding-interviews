e9f547bc3263adca931385cf664153d320ea62c9
nguyenthdat <nguyenthdat@outlook.com>
2022-01-16 10:20:48 +0000

    heaps

--- heaps/main.go
+++ heaps/main.go
@@ -18,20 +18,13 @@ func (h *MaxHeap) maxHeapifyUp(index int) {
 }
 
 // (index -1) /2 = parent index
-func parent(i int) int {
-	return (i - 1) / 2
-
-}
+func parent(i int) int { return (i - 1) / 2 }
 
 // index * 2 + 1 = (left child index)
-func left(i int) int {
-	return i*2 + 1
-}
+func left(i int) int { return i*2 + 1 }
 
 // index * 2 + 1 = (right child index)
-func right(i int) int {
-	return i*2 + 2
-}
+func right(i int) int { return i*2 + 2 }
 
 func (h *MaxHeap) swap(i1, i2 int) {
 	h.slice[i1], h.slice[i2] = h.slice[i2], h.slice[i1]
