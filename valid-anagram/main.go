9062614c002093806d97a28abeda68b3b2f8b1c6
nguyenthdat <nguyenthdat@outlook.com>
2022-01-16 09:14:39 +0000

    valid anagram

---
+++ valid-anagram/main.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(are_anagram("anagram", "nagaram"))
+}
+
+func are_anagram(s1, s2 string) bool {
+	if len(s1) != len(s2) {
+		return false
+	}
+	h1 := map[string]int{}
+	h2 := map[string]int{}
+	for _, s := range s1 {
+		if _, ok := h1[string(s)]; ok {
+			h1[string(s)] += 1
+		} else {
+			h1[string(s)] = 1
+		}
+	}
+	for _, s := range s2 {
+		if _, ok := h2[string(s)]; ok {
+			h2[string(s)] += 1
+		} else {
+			h2[string(s)] = 1
+		}
+
+	}
+	for k := range h1 {
+		if _, ok := h2[string(k)]; !ok || h1[k] != h2[k] {
+			return false
+		}
+	}
+	return true
+}
